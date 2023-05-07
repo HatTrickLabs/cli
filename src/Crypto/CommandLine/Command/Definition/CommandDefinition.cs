0ba0b3fb959635293483c2f1cee6006cd4e21788
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2023-05-07 17:43:19 -0500

    Split default command definition class out of command definition .cs file into it's own file

--- src/Crypto/CommandLine/Command/Definition/CommandDefinition.cs
+++ src/Crypto/CommandLine/Command/Definition/CommandDefinition.cs
@@ -171,16 +171,4 @@ namespace Crypto.CommandLine
         }
         #endregion
     }
-
-    public class DefaultCommandDefinition : CommandDefinition
-    {
-        #region constructors
-        public DefaultCommandDefinition() : base(name: DefaultCommandName)
-        { }
-
-        public DefaultCommandDefinition(string help, Action<Command> entryPoint, params CommandOptionDefinition[] options) 
-            : base(DefaultCommandName, help, entryPoint, options)
-        { }
-        #endregion
-    }
 }
---
+++ src/Crypto/CommandLine/Command/Definition/DefaultCommandDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Crypto.CommandLine
+{
+    public class DefaultCommandDefinition : CommandDefinition
+    {
+        #region constructors
+        public DefaultCommandDefinition() : base(name: DefaultCommandName)
+        { }
+
+        public DefaultCommandDefinition(string help, Action<Command> entryPoint, params CommandOptionDefinition[] options)
+            : base(DefaultCommandName, help, entryPoint, options)
+        { }
+        #endregion
+    }
+}
