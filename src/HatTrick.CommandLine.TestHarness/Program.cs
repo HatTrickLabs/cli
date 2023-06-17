ad50b15d8006c107a74b606cc53af886fc9a4e64
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2023-06-17 10:41:47 -0500

    Add empty test harness project.

---
+++ src/HatTrick.CommandLine.TestHarness/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HatTrick.CommandLine.TestHarness
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
