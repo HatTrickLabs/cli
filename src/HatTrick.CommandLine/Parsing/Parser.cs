2fd86b5a9981b56f38921e4a9db7fd62f6784c63
Jerrod Eiman <jerrod.eiman@hattricklabs.com>
2023-08-28 20:39:14 -0500

    Refactor Parser to have a collectable internal class

--- src/HatTrick.CommandLine/Parsing/Parser.cs
+++ src/HatTrick.CommandLine/Parsing/Parser.cs
@@ -6,92 +6,120 @@ namespace HatTrick.CommandLine.Parsing
     public static class Parser
     {
         #region parse
-        public static Command Parse(string[] tokens)
+        public static Command ParseCommand(string[] tokens)
         {
             if (tokens is null)
                 throw new ArgumentNullException(nameof(tokens));
 
-            if (tokens.Length == 0)
-                return new Command(CommandDefinition.DefaultCommandName);//optionless default command...
+            //no sense in holding a parser for multi-generations of GC...just use and release.
+            var instance = new Instance(tokens);
 
-            bool isDefault = tokens[0][0] == '-';//no command, just jumps right into option flags
-
-            int startAt = isDefault ? 0 : 1;
-            var opTokens = new ReadOnlySpan<string>(tokens, startAt, tokens.Length - startAt);
-
-            SetOf<CommandOption> options = ParseCommandOptions(opTokens);
-
-            string name = isDefault ? CommandDefinition.DefaultCommandName : tokens[0];
-            return new Command(name, options);
+            return instance.ParseCommand();
         }
         #endregion
 
-        #region parse command options
-        public static SetOf<CommandOption> ParseCommandOptions(ReadOnlySpan<string> tokens)
+        #region instance [class]
+        private sealed class Instance
         {
-            var ops = new SetOf<CommandOption>();
+            #region internals
+            private string[] _tokens;
+            #endregion
 
-            Func<string, bool> isExplicitAssign = (a) => a == "=" || a == ":";
-
-            string prev = null;
-            for (int i = 0; i < tokens.Length; i++)
+            #region constructors
+            internal Instance(string[] tokens)
             {
-                string token = tokens[i];
+                _tokens = tokens ?? throw new ArgumentNullException(nameof(tokens));
+            }
+            #endregion
 
-                if (isExplicitAssign(token))
+            #region parse command
+            internal Command ParseCommand()
+            {
+                string[] tokens = _tokens;
+
+                if (tokens.Length == 0)//optionless default command...
+                    return new Command(CommandDefinition.DefaultCommandName);
+
+                bool isDefault = tokens[0][0] == '-';//no command, just jumps right into option flags
+
+                int startAt = isDefault ? 0 : 1;
+                var opTokens = new ReadOnlySpan<string>(tokens, startAt, tokens.Length - startAt);
+
+                SetOf<CommandOption> options = this.ParseCommandOptions(opTokens);
+
+                string name = isDefault ? CommandDefinition.DefaultCommandName : tokens[0];
+                return new Command(name, options);
+            }
+            #endregion
+
+            #region parse command options
+            private SetOf<CommandOption> ParseCommandOptions(ReadOnlySpan<string> tokens)
+            {
+                var ops = new SetOf<CommandOption>();
+
+                var isExplicitAssign = (string tkn) => tkn == "=" || tkn == ":";
+
+                string prev = null;
+                for (int i = 0; i < tokens.Length; i++)
                 {
-                    //TODO: expand on this...it could be a syntax error...ie: copy :--from c:\test -to c:\test2
-                    //maybe check i%2 == 1
+                    string token = tokens[i];
+
+                    if (isExplicitAssign(token))
+                    {
+                        //TODO: expand on this...it could be a syntax error.
+                        //maybe check i % 2 == 1
+                        prev = token;
+                        continue;
+                    }
+
+                    //starts with '-' then must be option flag unless prev arg is an explicit assignment char
+                    if (token[0] == '-' && !isExplicitAssign(prev))
+                    {
+                        if (token.Length == 1)
+                            throw new CommandInputException(this.ExceptionMessageHelper(token, i));
+
+                        if (token[1] == '-') //verbose option flag
+                            ops.Add(new CommandOption(token));
+
+                        else if (token.Length == 2) //single terse option flag
+                            ops.Add(new CommandOption(token));
+
+                        else //must be a compound terse option flag, unroll into individual flags
+                            UnrollCompoundFlag(token, (f) => ops.Add(f));
+                    }
+                    else //no '-' and prev not an explicit assign, must be an option argument
+                    {
+                        if (ops.Length == 0)
+                            throw new CommandInputException($"{this.ExceptionMessageHelper(token, i)}...positional arguments not supported.");
+
+                        ops[^1].ApplyArgument(token);//apply current arg to the last op in the set
+                    }
+
                     prev = token;
-                    continue;
                 }
 
-                //starts with '-' then must be option flag unless prev arg is an explicit assignment char
-                if (token[0] == '-' && !isExplicitAssign(prev))
-                {
-                    if (token.Length == 1)
-                        throw new CommandInputException(Parser.ExceptionMessageHelper(token, i));
-
-                    if (token[1] == '-') //verbose option flag
-                        ops.Add(new CommandOption(token));
-
-                    else if (token.Length == 2) //single terse option flag
-                        ops.Add(new CommandOption(token));
-
-                    else //must be a compound terse option flag, unroll into individual flags
-                        UnrollCompoundFlag(token, (f) => ops.Add(f));
-                }
-                else //no '-' and prev not an explicit assign, must be an option argument
-                {
-                    if (ops.Length == 0)
-                        throw new CommandInputException($"{Parser.ExceptionMessageHelper(token, i)}...positional arguments not supported.");
-
-                    ops[^1].ApplyArgument(token);//apply current arg to the last op in the set
-                }
-
-                prev = token;
+                return ops;
             }
+            #endregion
 
-            return ops;
-        }
-        #endregion
-
-        #region unroll compound flag
-        private static void UnrollCompoundFlag(string flag, Action<CommandOption> onFlagUnrolled)
-        {
-            //start at idx 1 to skip the '-'
-            for (int i = 1; i < flag.Length; i++)
+            #region unroll compound flag
+            private void UnrollCompoundFlag(string flag, Action<CommandOption> onFlagUnrolled)
             {
-                var op = new CommandOption(flag: "-" + flag[i]);
-                onFlagUnrolled(op);
+                //start at idx 1 to skip the '-'
+                for (int i = 1; i < flag.Length; i++)
+                {
+                    var op = new CommandOption(flag: "-" + flag[i]);
+                    onFlagUnrolled(op);
+                }
             }
-        }
-        #endregion
+            #endregion
 
-        #region exception message helper
-        private static string ExceptionMessageHelper(string token, int index)
-        {
-            return $"Invalid command line argument provided: '{token}' at postition: {index + 1}";
+            #region exception message helper
+            private string ExceptionMessageHelper(string token, int index)
+            {
+                return $"Invalid command line argument provided: '{token}' at postition: {index + 1}";
+            }
+            #endregion
         }
         #endregion
     }
